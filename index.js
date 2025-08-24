6a401e2516016a0c2cc506cf73b554a656f4cc77
Sourabh <godarasourav181@gmail.com>
2025-08-24 21:50:24 +0530

    revert

--- index.js
+++ index.js
@@ -13,30 +13,13 @@ app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 2
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 
-const requestIp = require("request-ip");
 // inside middleware handler
-var ipMiddleware = function (req, res, next) {
-  const clientIp = requestIp.getClientIp(req);
-  next();
-};
-app.use(requestIp.mw());
 // on
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-app.get("/api/whoami", (req, res) => {
-  var ipaddress = req.clientIp;
-  var language = req.acceptsLanguages();
-  var software = req.get("User-Agent");
-  console.log(ipaddress);
-  res.json({
-    ipaddress: ipaddress,
-    language: language[0],
-    software: software.slice(software.indexOf("(") + 1, software.indexOf(")")),
-  });
-});
 // your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
